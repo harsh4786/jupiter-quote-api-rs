41afd2d1554fd1a0b795a1ca86b9b3fcdf2336f1
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2024-06-21 01:23:50 +0800

    new type for parsed price data

--- examples/quotes.rs
+++ examples/quotes.rs
@@ -15,7 +15,7 @@ async fn main() -> jup_ag::Result<()> {
     let ui_amount = 1.;
 
     for (output_token, output_decimals) in [(usdc, 6), (msol, 9), (sol, 9)] {
-        let data = jup_ag::price(sol, output_token, ui_amount).await?.data;
+        let data = jup_ag::price(sol, output_token).await?.data;
         println!(
             "Price for {} {} is {} {}",
             ui_amount, data.input_symbol, data.price, data.output_symbol
--- src/lib.rs
+++ src/lib.rs
@@ -152,11 +152,13 @@ where
 pub async fn price(
     input_mint: Pubkey,
     output_mint: Pubkey,
-) -> Result<PriceInfo> {
+) -> Result<HashMap<Pubkey, PriceInfo>> {
+    let mut price_map = HashMap::new();
     let url =
         format!("{PRICE_API_URL}/price?ids={input_mint}&vsToken={output_mint}");
     let price_info = reqwest::get(url).await?.json::<JupApiData>().await?.data.get(&input_mint.to_string()).ok_or(Error::JupiterApi("No price found".to_string()))?.to_owned();
-    Ok(price_info)
+    price_map.insert(input_mint, price_info);
+    Ok(price_map)
 }
 
 #[derive(Default)]
